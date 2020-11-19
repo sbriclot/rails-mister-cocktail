9c9cd323c9642d958786ee6cbb08c6ab977f5fc1
Stephane Briclot <stephane.briclot@gmail.com>
2020-11-19 13:39:18 +0100

    Mister Cocktail routing Cocktails

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+list = URI.open("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list")
+ingredients = JSON.load(list)["drinks"]
+puts "Start generating #{ingredients.count} ingredients..."
+
+ingredients.each do |i|
+  Ingredient.create!(name: i["strIngredient1"])
+end
+
+puts "Done"
