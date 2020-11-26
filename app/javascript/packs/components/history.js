ea707d5898f40192fe4c3b24a487f014912bdb37
Stephane Briclot <stephane.briclot@gmail.com>
2020-11-26 20:53:17 +0100

    Tets with TypedJS

--- app/javascript/packs/components/history.js
+++ app/javascript/packs/components/history.js
@@ -1,17 +1,19 @@
 const toggleHistory = () => {
   const btnHistory = document.getElementById("btn-history");
 
-  btnHistory.addEventListener('click', () => {
-    const history = document.getElementById("dsp-history");
-    history.classList.toggle("d-none");
-
-    if (btnHistory.innerText.includes("Show")) {
-      btnHistory.innerText = "Hide history"
-    }
-    else {
-      btnHistory.innerText = "Show history"
-    }
-  });
+  if (btnHistory) {
+    btnHistory.addEventListener('click', () => {
+      const history = document.getElementById("dsp-history");
+      history.classList.toggle("d-none");
+  
+      if (btnHistory.innerText.includes("Show")) {
+        btnHistory.innerText = "Hide history"
+      }
+      else {
+        btnHistory.innerText = "Show history"
+      }
+    });
+  }
 }
 
 export { toggleHistory };
